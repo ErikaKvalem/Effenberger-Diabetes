7acce250bb1d4cfe1b20a449076f49d0752ac63e
Erika Kvalem <erika.kvalem@gmail.com>
2025-01-09 15:52:01 +0100

    completed tutorial

--- 02-scripts/000_dada2.R
+++ 02-scripts/000_dada2.R
@@ -34,7 +34,103 @@ head(out)
 #Learn the Error Rates
 errF <- learnErrors(filtFs, multithread=TRUE)
 errR <- learnErrors(filtRs, multithread=TRUE)
-plotErrors(errF, nominalQ=TRUE)
+#plotErrors(errF, nominalQ=TRUE)
 
 #Sample Inference
 dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
+dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
+#inspect 
+dadaFs[[1]]
+
+#Merge paired reads 
+mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
+# Inspect the merger data.frame from the first sample
+head(mergers[[1]])
+
+#Construct sequence table
+seqtab <- makeSequenceTable(mergers)
+dim(seqtab)
+
+# Inspect distribution of sequence lengths
+table(nchar(getSequences(seqtab)))
+
+#Remove chimeras
+seqtab.nochim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE, verbose=TRUE)
+dim(seqtab.nochim)
+
+sum(seqtab.nochim)/sum(seqtab)
+
+#Track reads through the pipeline
+getN <- function(x) sum(getUniques(x))
+track <- cbind(out, sapply(dadaFs, getN), sapply(dadaRs, getN), sapply(mergers, getN), rowSums(seqtab.nochim))
+# If processing a single sample, remove the sapply calls: e.g. replace sapply(dadaFs, getN) with getN(dadaFs)
+colnames(track) <- c("input", "filtered", "denoisedF", "denoisedR", "merged", "nonchim")
+rownames(track) <- sample.names
+head(track)
+
+
+#Assign taxonomy
+taxa <- assignTaxonomy(seqtab.nochim, "/data/projects/2024/Effenberger-Diabetes/data/20011/Fastq/silva/silva_nr99_v138.2_toSpecies_trainset.fa.gz", multithread=TRUE)
+
+
+#inspect taxonomic assignment
+taxa.print <- taxa # Removing sequence rownames for display only
+rownames(taxa.print) <- NULL
+head(taxa.print)
+
+#Handoff to phyloseq
+library(phyloseq); packageVersion("phyloseq")
+library(Biostrings); packageVersion("Biostrings")
+library(ggplot2); packageVersion("ggplot2")
+
+theme_set(theme_bw())
+
+# simple sample data.frame from the information encoded in the filenames. 
+samdf <- read.csv(paste0(path,"/20011_SampleInfo.csv"))
+samples.out <- rownames(seqtab.nochim)
+# Assuming your dataframe is named 'df'
+samdf <- rbind(samdf, data.frame(IMGM.ID = "20002-0053", Sample.Information = "Control", Type = "Control"))
+# Assuming your dataframe is named 'df'
+samdf <- rbind(samdf, data.frame(IMGM.ID = "20002-0054", Sample.Information = "Control", Type = "Control"))
+rownames(samdf) <- samdf$IMGM.ID
+rownames(samdf) <- samples.out
+
+#construct a phyloseq object directly from the dada2 outputs.
+ps <- phyloseq(otu_table(seqtab.nochim, taxa_are_rows=FALSE), 
+               sample_data(samdf), 
+               tax_table(taxa))
+ps <- prune_samples(sample_names(ps) != "Mock", ps) # Remove mock sample
+
+
+dna <- Biostrings::DNAStringSet(taxa_names(ps))
+names(dna) <- taxa_names(ps)
+ps <- merge_phyloseq(ps, dna)
+taxa_names(ps) <- paste0("ASV", seq(ntaxa(ps)))
+ps
+
+#Visualize alpha-diversity:
+plot_richness(ps, x="Type", measures=c("Shannon", "Simpson"), color="Sample.Information")
+
+
+## Transform data to proportions as appropriate for Bray-Curtis distances
+ps.prop <- transform_sample_counts(ps, function(otu) otu/sum(otu))
+
+# Access the OTU table
+otu_table_matrix <- as.matrix(ps.prop@otu_table)
+
+# Replace NaN with 0
+otu_table_matrix[is.nan(otu_table_matrix)] <- 0
+
+# Update the OTU table in the phyloseq object
+ps.prop@otu_table <- otu_table(otu_table_matrix, taxa_are_rows = taxa_are_rows(ps.prop))
+
+ord.nmds.bray <- ordinate(ps.prop, method="NMDS", distance="bray")
+
+plot_ordination(ps.prop, ord.nmds.bray, color="Sample.Information", title="Bray NMDS")
+
+#Bar plot:
+top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
+ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
+ps.top20 <- prune_taxa(top20, ps.top20)
+plot_bar(ps.top20, x="Sample.Information", fill="Species")   + facet_wrap(~Type, scales="free_y")
+
