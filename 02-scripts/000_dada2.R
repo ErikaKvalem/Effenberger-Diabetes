0b0d6053baf8127983273dba1b95327747124f1b
Erika Kvalem <erika.kvalem@gmail.com>
2025-03-19 09:36:19 +0100

    working on the analysis

--- 02-scripts/000_dada2.R
+++ 02-scripts/000_dada2.R
@@ -80,9 +80,9 @@ taxa.print <- taxa # Removing sequence rownames for display only
 rownames(taxa.print) <- NULL
 head(taxa.print)
 
-`%not_in%` <- purrr::negate(`%in%`)
+#`%not_in%` <- purrr::negate(`%in%`)
 df_taxa <- as.data.frame(taxa, stringsAsFactors = FALSE)
-
+#
 df_taxa_f=df_taxa[!is.na(df_taxa$Kingdom),]
 df_taxa_f=df_taxa[!is.na(df_taxa$Phylum),]
 df_taxa_f=df_taxa[!is.na(df_taxa$Class),]
@@ -112,30 +112,34 @@ rownames(samdf) <- samples.out
 
 samdf_filtered <- samdf %>%
   filter(!grepl("Control|Kontrolle", Type))
+# Modify IMGM.ID: Replace "-" with "_" and add "s" at the beginning
+samdf_filtered$IMGM.ID <- paste0("s", gsub("-", "_", samdf_filtered$IMGM.ID))
 
-
+# Set the modified IMGM.ID as row names
+rownames(samdf_filtered) <- samdf_filtered$IMGM.ID
+#
 otu_names <- colnames(seqtab.nochim)
-
-# Taxa names from df_taxa_f (assuming they are set as rownames)
+#
+## Taxa names from df_taxa_f (assuming they are set as rownames)
 taxa_names <- rownames(df_taxa_f)
-
-# See which names are common
+#
+## See which names are common
 common_taxa <- intersect(otu_names, taxa_names)
-
+#
 common_taxa <- intersect(colnames(seqtab.nochim), rownames(df_taxa_f))
-
-# Check if any common taxa exist
+#
+## Check if any common taxa exist
 if (length(common_taxa) == 0) {
   stop("No matching taxa names found between the OTU table and taxonomy table.")
 }
-
-# Subset the OTU table to keep only columns corresponding to common taxa
+#
+## Subset the OTU table to keep only columns corresponding to common taxa
 seqtab.nochim_filtered <- seqtab.nochim[, common_taxa, drop = FALSE]
-
-# Subset the taxonomy table to keep only rows corresponding to common taxa
+#
+## Subset the taxonomy table to keep only rows corresponding to common taxa
 df_taxa_f_filtered <- df_taxa_f[common_taxa, , drop = FALSE]
-
-# Convert the filtered taxonomy data frame to a matrix
+#
+## Convert the filtered taxonomy data frame to a matrix
 tax_mat <- as.matrix(df_taxa_f_filtered)
 
 ps <- phyloseq(
@@ -174,7 +178,11 @@ ps.prop@otu_table <- otu_table(otu_table_matrix, taxa_are_rows = taxa_are_rows(p
 plot_ordination(ps.prop, ord.nmds.bray, color="Sample.Information", title="Bray NMDS")
 
 #Bar plot:
-top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:20]
+
+
+top20 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:100]
+
+
 ps.top20 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
 ps.top20 <- prune_taxa(top20, ps.top20)
 plot_bar(ps.top20, x="Family", fill="Sample.Information")   + facet_wrap(~Type, scales="free_y")
@@ -185,18 +193,56 @@ plot_bar(ps.top20, x="Species", fill="Sample.Information")   + facet_wrap(~Type,
 
 plot_bar(ps.top20, fill="Genus", x="Sample.Information") 
 
+
 library(phyloseq)
 library(ggplot2)
 
 # Convert phyloseq object to a data frame for ggplot2
 ps_melt <- psmelt(ps.top20)
 
-# Create a box plot
-ggplot(ps_melt, aes(x = Type, y = Abundance, fill = Sample.Information)) +
-  geom_boxplot() +
-  theme_minimal() +
-  labs(x = "Sample Information", y = "Abundance", title = "Box Plot of Abundance by Genus") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 0.3))  # Rotate x-axis labels if needed
 
 
+###############################################################
+scale_y_continuous(labels = scales::percent)
+library(scales)
 
+get_taxa_unique(ps, "Family")
+sample_data(ps)$Type<-factor(sample_data(ps)$Type)
+sample_data(ps)$Sample.Information<-factor(sample_data(ps)$Sample.Information)
+
+ps
+
+
+phy2 <- ps %>% aggregate_taxa(level = "Phylum") %>%
+  
+  microbiome::transform(transform = "compositional")
+
+phy2 %>%  plot_composition()+
+  scale_y_continuous(labels = scales::percent) 
+
+phy2 %>% 
+  plot_composition() +
+  scale_y_continuous(labels = scales::percent) +
+  labs(color = "Phylum", fill = "Phylum")
+
+phy2 %>%  plot_composition(group_by = "Type")+ scale_y_continuous(labels = percent)
+
+
+# Remove taxa where Phylum is NA or "Unknown"
+
+# Remove taxa where any rank has "Unknown" or NA
+phy2_clean <- subset_taxa(phy2, apply(tax_table(phy2), 1, function(x) all(!is.na(x) & x != "Unknown")))
+
+#phy2_clean <- subset_taxa(phy2, !is.na(Kingdom) & Kingdom != "Unknown")
+#phy2_clean <- subset_taxa(phy2, !is.na(Phylum) & Phylum != "Unknown")
+#phy2_clean <- subset_taxa(phy2, !is.na(Class) & Class != "Unknown")
+#phy2_clean <- subset_taxa(phy2, !is.na(Order) & Order != "Unknown")
+#phy2_clean <- subset_taxa(phy2, !is.na(Family) & Family != "Unknown")
+
+
+phy2_clean %>%  plot_composition()+
+  scale_y_continuous(labels = scales::percent) 
+
+############################### out of ampliseq pipeline 
+
+ps = readRDS("/data/projects/2024/Effenberger-Diabetes/out/nf_core_ampliseq/phyloseq/dada2_phyloseq.rds")
