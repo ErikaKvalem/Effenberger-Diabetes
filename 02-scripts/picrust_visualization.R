96fc1b207af903a86486917a94979c2ebc693703
Erika Kvalem <erika.kvalem@gmail.com>
2025-04-16 11:43:58 +0200

    patched function working flow

--- 02-scripts/picrust_visualization.R
+++ 02-scripts/picrust_visualization.R
@@ -14,7 +14,6 @@ library(patchwork)
 
 # Load necessary data: abundance data and metadata
 
-
 abundance_file <- "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2_out_pipeline/KO_metagenome_out/filtered_abundance.tsv"
 abundance_df <- read_delim(
   abundance_file,
@@ -31,92 +30,7 @@ metacyc_abundance_df <- read_delim(
   trim_ws = TRUE
 )
 
-
-# Ensure it's a data.frame
-#metadata <- as.data.frame(metadata)
-
-# Filter metadata for the two groups
-#metadata_filtered <- metadata[metadata$Type %in% c("pankreopriver Diabetes", "Diabetes mellitus Typ1"), ]
-
-#write.table(metadata_filtered, "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/tables/original/metadata_filtered.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
-
-metadata2 <- read_delim(
-  "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/tables/original/20011_SampleInfo.csv",
-  delim = ",",
-  escape_double = FALSE,
-  trim_ws = TRUE
-)
-
-
-# Get matching sample IDs
-#selected_samples <- metadata_filtered$`IMGM ID`
-
-#abundance_filtered <- abundance_df[, c("function", selected_samples)]
-#write.table(abundance_filtered, "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2_out_pipeline/KO_metagenome_out/filtered_abundance.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
-
-# Filter abundance data: keep only 'function' column + selected samples
-#metacyc_abundance_df_filtered <- metacyc_abundance_df[, c("function", selected_samples)]
-#write.table(metacyc_abundance_df_filtered, "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2_out_pipeline/EC_metagenome_out/metacyc_abundance_filtered.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
-
-
-
-
-
-# If you want to analyze the abundance of KEGG pathways instead of KO within the pathway, please set `ko_to_kegg` to TRUE.
-# KEGG pathways typically have more descriptive explanations.
-
-
-
-# Run ggpicrust2 with input file path
-results_file_input <- ggpicrust2(file = abundance_file,
-                                 metadata = metadata,
-                                 group = "Type", # For example dataset, group = "Environment"
-                                 pathway = "KO",
-                                 daa_method = "DESeq2",
-                                 ko_to_kegg = TRUE,
-                                 order = "pathway_class",
-                                 p_values_bar = TRUE,
-                                 x_lab = "pathway_name")
-
-# Run ggpicrust2 with imported data.frame
-abundance_data <- read_delim(abundance_file, delim = "\t", col_names = TRUE, trim_ws = TRUE)
-
-# Run ggpicrust2 with input data
-results_data_input <- ggpicrust2(data = abundance_data,
-                                 metadata = metadata,
-                                 group = "Type", # For example dataset, group = "Environment"
-                                 pathway = "KO",
-                                 daa_method = "LinDA",
-                                 ko_to_kegg = TRUE,
-                                 order = "pathway_class",
-                                 p_values_bar = TRUE,
-                                 x_lab = "pathway_name")
-
-# Access the plot and results dataframe for the first DA method
-example_plot <- results_file_input[[1]]$plot
-example_results <- results_file_input[[1]]$results
-
-
-# Analyze the EC or MetaCyc pathway
-
-results_file_input <- ggpicrust2(
-  data = metacyc_abundance_df,
-  metadata = metadata,
-  group = "Type",
-  pathway = "MetaCyc",
-  daa_method = "ALDEx2",
-  ko_to_kegg = FALSE,
-  order = "group",
-  p_values_bar = FALSE,  # Disable plotting for now
-  x_lab = "description"
-)
-results_file_input[[1]]$plot
-results_file_input[[1]]$results
-
-
-
-
-#############################################################################################
+ec_name_map <- read.delim("/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2/default_files/description_mapfiles/ec_name.txt.gz", header = FALSE, sep = "\t", col.names = c("feature", "description"))
 metadata <- read_delim(
   "/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/tables/original/20011_SampleInfo.csv",
   delim = ",",
@@ -125,9 +39,11 @@ metadata <- read_delim(
 )
 
 
-
 kegg_abundance <- ko2kegg_abundance("/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2_out_pipeline/KO_metagenome_out/pred_metagenome_unstrat.tsv")
 
+
+
+############################################################################################# KEGG barplot
 metadata_filtered <- metadata[metadata$Type %in% c("Diabetes mellitus Typ1", "pankreopriver Diabetes"), ]
 
 # If sample IDs are in 'SampleID' column
@@ -147,22 +63,184 @@ filtered_df <- daa_annotated_sub_method_results_df[grepl("Metabolism;", daa_anno
 
 p <- pathway_errorbar(abundance = kegg_abundance_filtered, daa_results_df = filtered_df, Group = metadata_filtered$Type, p_values_threshold = 0.01, order = "pathway_class", select = NULL, ko_to_kegg = TRUE, p_value_bar = TRUE, colors = c("#3685BC","#D93C3E"), x_lab = "pathway_name")
 
+############################################################################################# MetaCyc Pathway and EC
 
-# Workflow for MetaCyc Pathway and EC
+
+
+
+metadata_filtered <- metadata[metadata$Type %in% c("Diabetes mellitus Typ1", "pankreopriver Diabetes"), ]
+
+# If sample IDs are in 'SampleID' column
+sample_ids <- metadata_filtered$`IMGM ID`
+
 
 metacyc_abundance <-  read_delim("/data/scratch/kvalem/projects/2024/Effenberger-Diabetes/02-scripts/picrust2-2.6.1/picrust2_out_pipeline/EC_metagenome_out/pred_metagenome_unstrat.tsv", delim = "\t",
                                  escape_double = FALSE,
                                  trim_ws = TRUE)
 metacyc_abundance_filtered <- metacyc_abundance[, c(TRUE, colnames(metacyc_abundance)[-1] %in% sample_ids)]
 metacyc_abundance_filtered <- metacyc_abundance_filtered[, colnames(metacyc_abundance_filtered) != "s20011_0001"]
+names(metacyc_abundance_filtered)[names(metacyc_abundance_filtered) == "function"] <- "pathway"
 
-metacyc_daa_results_df <- pathway_daa(abundance = metacyc_abundance_filtered %>% column_to_rownames("function"), metadata = metadata_filtered, group = "Type", daa_method = "DESeq2")
+metacyc_daa_results_df <- pathway_daa(abundance = metacyc_abundance_filtered %>% column_to_rownames("pathway"), metadata = metadata_filtered, group = "Type", daa_method = "DESeq2")
 metacyc_daa_annotated_results_df <- pathway_annotation(pathway = "MetaCyc", daa_results_df = metacyc_daa_results_df, ko_to_kegg = FALSE)
-daa_sub_method_metacyc_daa_annotated_results_df<- metacyc_daa_annotated_results_df[metacyc_daa_annotated_results_df$method == "DESeq2" , ]
+daa_sub_method_metacyc_daa_annotated_results_df <- metacyc_daa_annotated_results_df[metacyc_daa_annotated_results_df$method == "DESeq2" , ]
 
 
-#pathway_errorbar(abundance = metacyc_abundance_filtered %>% column_to_rownames("function"), daa_results_df = daa_sub_method_metacyc_daa_annotated_results_df, Group = metadata_filtered$Type, ko_to_kegg = FALSE, p_values_threshold = 0.05, order = "group", select = NULL, p_value_bar = TRUE, colors = NULL, x_lab = "description")
+filtered_daa_results_annotated <- metacyc_daa_annotated_results_df %>%
+  left_join(ec_name_map, by = "feature")
+
+filtered_daa_results_annotated <- filtered_daa_results_annotated %>%
+  mutate(description = description.y) %>%
+  select(-description.x, -description.y)
 
 
-feature_with_p_0.05 <- metacyc_daa_results_df %>% filter(p_adjust < 0.05)
-pathway_heatmap(abundance = metacyc_daa_results_df %>% filter("function" %in% feature_with_p_0.05$feature) %>% column_to_rownames("function"), metadata = metadata_filtered, group = "Type")
+metacyc_abundance_filtered_named <- metacyc_abundance_filtered %>%
+  column_to_rownames("pathway")  # or "pathway" or "description", depending on column name
+
+top_features <- filtered_daa_results_annotated %>%
+  arrange(p_adjust) %>%
+  slice_head(n = 30) %>%
+  pull(feature)
+
+pathway_errorbar_patched <- ggpicrust2::pathway_errorbar
+
+pathway_errorbar_patched <- function (abundance, daa_results_df, Group, ko_to_kegg = FALSE, 
+                                      p_values_threshold = 0.05, order = "group", select = NULL, 
+                                      p_value_bar = TRUE, colors = NULL, x_lab = NULL) 
+{
+  if (!is.matrix(abundance) && !is.data.frame(abundance)) {
+    stop("'abundance' must be a matrix or data frame")
+  }
+  if (!is.data.frame(daa_results_df)) {
+    stop("'daa_results_df' must be a data frame")
+  }
+  required_cols <- c("feature", "method", "group1", "group2", "p_adjust")
+  missing_cols <- setdiff(required_cols, colnames(daa_results_df))
+  if (length(missing_cols) > 0) {
+    stop("Missing required columns in daa_results_df: ", paste(missing_cols, collapse = ", "))
+  }
+  if (length(Group) != ncol(abundance)) {
+    stop("Length of Group must match number of columns in abundance matrix")
+  }
+  
+  # ⛔ method check REMOVED
+  
+  if (nlevels(factor(daa_results_df$group1)) != 1 || nlevels(factor(daa_results_df$group2)) != 1) {
+    stop("The 'group1' or 'group2' column in the 'daa_results_df' data frame contains more than one group. Please filter each to contain only one group.")
+  }
+  
+  if (is.null(x_lab)) {
+    x_lab <- ifelse(ko_to_kegg, "pathway_name", "description")
+  }
+  
+  if (!(x_lab %in% colnames(daa_results_df))) {
+    stop("The 'x_lab' you defined does not exist as a column in the 'daa_results_df' data frame.")
+  }
+  
+  daa_results_df <- daa_results_df[!is.na(daa_results_df[, x_lab]), ]
+  daa_results_filtered_df <- daa_results_df[daa_results_df$p_adjust < p_values_threshold, ]
+  
+  if (!is.null(select)) {
+    daa_results_filtered_sub_df <- daa_results_filtered_df[daa_results_filtered_df$feature %in% select, ]
+  } else {
+    daa_results_filtered_sub_df <- daa_results_filtered_df
+  }
+  
+  if (nrow(daa_results_filtered_sub_df) > 30) {
+    stop("Too many significant features (>30). Use 'select' to reduce the number of features.")
+  }
+  if (nrow(daa_results_filtered_sub_df) == 0) {
+    stop("No significant features found.")
+  }
+  
+  relative_abundance_mat <- apply(t(as.matrix(abundance)), 1, function(x) x / sum(x))
+  sub_relative_abundance_mat <- relative_abundance_mat[rownames(relative_abundance_mat) %in% daa_results_filtered_sub_df$feature, ]
+  error_bar_matrix <- cbind(sample = colnames(sub_relative_abundance_mat), group = Group, t(sub_relative_abundance_mat))
+  error_bar_df <- as.data.frame(error_bar_matrix)
+  error_bar_df$group <- factor(Group, levels = levels(as.factor(Group)))
+  error_bar_pivot_longer_df <- tidyr::pivot_longer(error_bar_df, -c(sample, group))
+  error_bar_pivot_longer_tibble <- dplyr::mutate(error_bar_pivot_longer_df,
+                                                 group = as.factor(group),
+                                                 sample = factor(sample),
+                                                 name = factor(name),
+                                                 value = as.numeric(value))
+  
+  error_bar_summary <- error_bar_pivot_longer_tibble %>%
+    dplyr::group_by(name, group) %>%
+    dplyr::summarise(mean = mean(value), sd = stats::sd(value), .groups = "drop")
+  
+  if (order == "group") {
+    daa_results_filtered_sub_df$pro <- 1
+    for (i in levels(error_bar_summary$name)) {
+      sub <- error_bar_summary[error_bar_summary$name == i, ]
+      top_group <- as.vector(sub[sub$mean == max(sub$mean), ]$group)
+      daa_results_filtered_sub_df[daa_results_filtered_sub_df$feature == i, ]$pro <- top_group
+    }
+    order_idx <- order(daa_results_filtered_sub_df$pro, daa_results_filtered_sub_df$p_adjust)
+  } else if (order == "p_values") {
+    order_idx <- order(daa_results_filtered_sub_df$p_adjust)
+  } else {
+    order_idx <- order(daa_results_filtered_sub_df$feature)
+  }
+  
+  daa_results_filtered_sub_df <- daa_results_filtered_sub_df[order_idx, ]
+  error_bar_summary_ordered <- do.call(rbind, lapply(daa_results_filtered_sub_df$feature, function(i) {
+    error_bar_summary[error_bar_summary$name == i, ]
+  }))
+  
+  error_bar_summary_ordered[, x_lab] <- rep(daa_results_filtered_sub_df[, x_lab], each = length(unique(error_bar_summary$group)))
+  error_bar_summary_ordered$name <- factor(error_bar_summary_ordered$name, levels = rev(daa_results_filtered_sub_df$feature))
+  
+  if (is.null(colors)) {
+    colors <- c("#d93c3e", "#3685bc", "#6faa3e", "#e8a825", "#c973e6", "#ee6b3d", "#2db0a7", "#f25292")[1:nlevels(as.factor(Group))]
+  }
+  
+  error_bar_summary_ordered$name <- factor(
+    daa_results_filtered_sub_df[[x_lab]][match(as.character(error_bar_summary_ordered$name),
+                                               as.character(daa_results_filtered_sub_df$feature))],
+    levels = rev(daa_results_filtered_sub_df[[x_lab]])
+  )
+  
+  # Debug print to confirm!
+  print("✅ Label mapping applied:")
+  print(head(error_bar_summary_ordered$name))
+  
+  bar_plot <- ggplot2::ggplot(error_bar_summary_ordered,
+                              ggplot2::aes(mean, name, fill = group)) +
+    ggplot2::geom_errorbar(ggplot2::aes(xmax = mean + sd, xmin = 0),
+                           position = ggplot2::position_dodge(width = 0.8),
+                           width = 0.5, linewidth = 0.5, color = "black") +
+    ggplot2::geom_bar(stat = "identity", position = ggplot2::position_dodge(width = 0.8), width = 0.8) +
+    ggplot2::scale_fill_manual(values = colors) +
+    ggplot2::labs(x = "Relative Abundance", y = NULL) +
+    ggplot2::theme_minimal() +
+    ggplot2::theme(legend.position = "top",
+                   axis.text.y = ggplot2::element_text(size = 10, color = "black"),
+                   axis.title.x = ggplot2::element_text(size = 10, color = "black"))
+
+  
+  
+  
+  
+  return(bar_plot)
+}
+
+
+
+
+
+p2 <- pathway_errorbar_patched(
+  abundance = metacyc_abundance_filtered_named,
+  daa_results_df = filtered_daa_results_annotated,
+  Group = metadata_filtered$Type,
+  ko_to_kegg = FALSE,
+  p_values_threshold = 0.05,
+  order = "group",
+  select = top_features,
+  p_value_bar = TRUE,
+  colors = NULL,
+  x_lab = "description"
+)
+
+
+################################################################################
